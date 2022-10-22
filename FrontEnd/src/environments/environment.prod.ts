01948d9c1be0645024a5a60a76325ee7aa438468
Julian <julypuebla@gmail.com>
2022-10-22 19:57:29 -0300

    Cargo Firebase Storage
    Realizo ultima parte de imagenes

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fpbfront',
+    appId: '1:424201364636:web:ef62e2f364d16a029df959',
+    storageBucket: 'fpbfront.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCUTTtO4UZZZgztoE9bhISfczWtI8T2Q9E',
+    authDomain: 'fpbfront.firebaseapp.com',
+    messagingSenderId: '424201364636',
+  },
   production: true
 };
--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fpbfront',
+    appId: '1:424201364636:web:ef62e2f364d16a029df959',
+    storageBucket: 'fpbfront.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCUTTtO4UZZZgztoE9bhISfczWtI8T2Q9E',
+    authDomain: 'fpbfront.firebaseapp.com',
+    messagingSenderId: '424201364636',
+  },
   production: false
 };
 
